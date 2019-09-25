4a16cce010bae3d3b96995c855651e4c35bada94
Kyle Richardson <kyle.richardson1@gmail.com>
2019-09-25 10:59:13 -0500

    logo in correct place

--- src/Header.js
+++ src/Header.js
@@ -2,19 +2,23 @@ import React from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope} from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import * as githubLogo from "./img/githublogo.png"
+import * as lambdaLogo from "./img/lambdalogo.png"
 
 
 
 const Header = (props) => {
     return (
         <div className="header">
-            <img src="./img/lambdalogo.png" alt="lambda"/>
-            <img src="./img/githublogo.png" alt="github"/>
-            <h1>Github Network</h1>
-            <h2>Kyle Richardson</h2>
-            <a href="mailto:kyle.richardson1@gmail.com"><FontAwesomeIcon className="icon" icon={faEnvelope}/></a>
-            <a href="https://github.com/kyle-richardson"><FontAwesomeIcon className="icon" icon={faGithub} /></a>
-            <a href="https://linkedin.com/in/kyle-m-richardson"><FontAwesomeIcon className="icon" icon={faLinkedin}/></a>
+            <img className="lambda-logo" src={lambdaLogo} alt="lambda"/>
+            <div className="title">
+                <h1>Github Network</h1>
+                <h2>Kyle Richardson</h2>
+                <a href="mailto:kyle.richardson1@gmail.com"><FontAwesomeIcon className="icon" icon={faEnvelope}/></a>
+                <a href="https://github.com/kyle-richardson"><FontAwesomeIcon className="icon" icon={faGithub} /></a>
+                <a href="https://linkedin.com/in/kyle-m-richardson"><FontAwesomeIcon className="icon" icon={faLinkedin}/></a>
+            </div>
+            <img className="github-logo" src={githubLogo} alt="github"/>
             
         </div>
     )
