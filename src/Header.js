88500af35c460a72b838839d46b32fd47bbfb0b7
Kyle Richardson <kyle.richardson1@gmail.com>
2019-09-25 10:40:07 -0500

    images

--- src/Header.js
+++ src/Header.js
@@ -8,9 +8,11 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 const Header = (props) => {
     return (
         <div className="header">
+            <img src="./img/lambdalogo.png" alt="lambda"/>
+            <img src="./img/githublogo.png" alt="github"/>
             <h1>Github Network</h1>
             <h2>Kyle Richardson</h2>
-            <a href = "mailto:kyle.richardson1@gmail.com" ><FontAwesomeIcon className="icon" icon={faEnvelope}/></a>
+            <a href="mailto:kyle.richardson1@gmail.com"><FontAwesomeIcon className="icon" icon={faEnvelope}/></a>
             <a href="https://github.com/kyle-richardson"><FontAwesomeIcon className="icon" icon={faGithub} /></a>
             <a href="https://linkedin.com/in/kyle-m-richardson"><FontAwesomeIcon className="icon" icon={faLinkedin}/></a>
             
