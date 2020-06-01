4c024cc0bd37ea5c2423b53f2040643a8e7f53b8
Kyle Richardson <kyle.richardson1@gmail.com>
2020-05-31 20:50:53 -0500

    remove lambda logo

--- src/Header.js
+++ src/Header.js
@@ -1,33 +1,29 @@
-import React from "react"
-import * as githubLogo from "./img/githublogo.png"
-import * as lambdaLogo from "./img/lambdalogo.png"
-
-
+import React from "react";
+import * as githubLogo from "./img/githublogo.png";
+import * as lambdaLogo from "./img/lambdalogo.png";
 
 const Header = (props) => {
-    return (
-        <div className="header">
-            <img className="github-logo" src={githubLogo} alt="github"/>
-            <div className="title">
-                <h1>Github Network</h1>
-                <form onSubmit = {props.handleSubmit}>
-                    <input
-                        className="text-input"
-                        name="searchValue"
-                        type='text' 
-                        value={props.searchValue}
-                        onChange={props.handleChange}
-                        placeholder="Search Github username"
-                    />
-                    <br />
-                    <button className="search-button">Search</button>
-                </form>
-                <h2>{props.mainUser.name}</h2>
-            </div>
-            <img className="lambda-logo" src={lambdaLogo} alt="lambda"/>
-            
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <img className="github-logo" src={githubLogo} alt="github" />
+      <div className="title">
+        <h1>Github Network</h1>
+        <form onSubmit={props.handleSubmit}>
+          <input
+            className="text-input"
+            name="searchValue"
+            type="text"
+            value={props.searchValue}
+            onChange={props.handleChange}
+            placeholder="Search Github username"
+          />
+          <br />
+          <button className="search-button">Search</button>
+        </form>
+        <h2>{props.mainUser.name}</h2>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
